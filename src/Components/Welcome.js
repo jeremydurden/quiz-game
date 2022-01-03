96ef2432be4146f993692a3dd71ca265cb6294f0
Jeremy Durden <jeremydurden@gmail.com>
2022-01-02 16:03:48 -0800

    fetching quiz data and setting up state

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,11 +1,13 @@
-function Welcome() {
+function Welcome({ quizButton }) {
   return (
     <div className="welcome--container">
       <h1 className="welcome--title">Quizzical</h1>
       <h3 className="welcome--sub-title">
         How many questions can you get right?
       </h3>
-      <button className="welcome--button">Start Quiz</button>
+      <button onClick={quizButton} className="welcome--button">
+        Start Quiz
+      </button>
     </div>
   );
 }
