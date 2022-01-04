77b02d207c9940bab4f9e623e349357fb87cb447
Jeremy Durden <jeremydurden@gmail.com>
2022-01-03 18:10:31 -0800

    logic to shuffle the array of answers

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -7,10 +7,15 @@ function Question({ question, answers }) {
     );
   });
 
+  let shuffledArray = answersArray
+    .map((value) => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
+
   return (
     <div className="question--container">
       <h2 className="question--h2">{question}</h2>
-      <div className="question--answer-container">{answersArray}</div>
+      <div className="question--answer-container">{shuffledArray}</div>
     </div>
   );
 }
